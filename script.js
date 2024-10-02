cbdf9fdfc4750e78cd6f510361698b3ba4f7870b
Hasanyos <hasanlord.lh@gmail.com>
2024-10-02 22:23:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const questions = [ /* List of questions here */ ];
+const questions = [
+    { question: "Are you more of a leader?", options: ["Yes", "No"] },
+    { question: "Do you enjoy planning?", options: ["Yes", "No"] },
+    // Add all your questions here
+];
+
 let currentQuestion = 0;
 let answers = [];
 
@@ -26,6 +31,5 @@ function answer(choice) {
 }
 
 function showResults() {
-    // Logic to calculate scores and display results
     document.querySelector(".container").innerHTML = `<h2>Your results are here</h2>`;
 }
