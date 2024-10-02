820a978c0242b1cd811fade10ecbbd696c2fa061
Hasanyos <hasanlord.lh@gmail.com>
2024-10-02 22:18:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const questions = [ /* List of questions here */ ];
+let currentQuestion = 0;
+let answers = [];
+
 function startTest() {
-    alert("This is a placeholder function. You can add your test logic here.");
+    document.querySelector(".container").innerHTML = getQuestionHTML(currentQuestion);
+}
+
+function getQuestionHTML(questionIndex) {
+    return `
+        <div class="question">
+            <h2>${questions[questionIndex].question}</h2>
+            ${questions[questionIndex].options.map((opt, i) =>
+            `<button onclick="answer(${i})">${opt}</button>`).join('')}
+        </div>`;
+}
+
+function answer(choice) {
+    answers.push(choice);
+    currentQuestion++;
+    if (currentQuestion < questions.length) {
+        document.querySelector(".container").innerHTML = getQuestionHTML(currentQuestion);
+    } else {
+        showResults();
+    }
+}
+
+function showResults() {
+    // Logic to calculate scores and display results
+    document.querySelector(".container").innerHTML = `<h2>Your results are here</h2>`;
 }
